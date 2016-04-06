d5e4c5c860a0dbd484d58bf10788e3a12477cf8d
Stephen Kell <stephen.kell@cl.cam.ac.uk>
2016-04-06 17:34:18 +0100

    cabs2cil: don't let extern inline prototypes replace definitions of the same function seen earlier.
    Add test case for repeated prototypes of extern inline functions.
    This commits merges the following two commits
    - stephenrkell/cil@bf49a9842a0a1d4fa4c4832e1b8e81288aca1b57
    - stephenrkell/cil@a9f7012ba608c068cc38fc4d15b68fbf4a14192b

--- test/testcil.pl
+++ test/testcil.pl
@@ -714,6 +714,7 @@ addTest("runall/alpha");
 addTest("testrun/blockattr2 USECFG=1");
 addTest("testrun/extinline2"); #TODO: fails starting at least from gcc 6.3, maybe earlier
 addTest("test/extinline3");
+addTest("test/extinline4");
 addTest("testrun/bool");
 addTest("testrun/booleanOp USE_LOGICAL_OPERATORS=1");
 addTest("test/va_arg_pack"); #TODO: fails starting at least from gcc 6.3, maybe earlier
